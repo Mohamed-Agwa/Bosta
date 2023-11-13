8fa7ed40ac54b06e87df26da0c20b6cebc271991
Mohamed-Agwa <moh.agwa1@gmail.com>
2023-11-13 13:04:49 +0200

    Updates

---
+++ precache-manifest.7c6b9232fcf6837a5b3f661f868c0bb4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f151473f366bf4e61bb61b383d9faa",
+    "url": "/Bosta/index.html"
+  },
+  {
+    "revision": "f2b4d5252e18afa79aa8",
+    "url": "/Bosta/static/css/2.52f2db83.chunk.css"
+  },
+  {
+    "revision": "52fee876f4b057dbcb45",
+    "url": "/Bosta/static/css/main.8350b68b.chunk.css"
+  },
+  {
+    "revision": "f2b4d5252e18afa79aa8",
+    "url": "/Bosta/static/js/2.0a276d6b.chunk.js"
+  },
+  {
+    "revision": "54de2502c5fc44f9134f0a957b2559ec",
+    "url": "/Bosta/static/js/2.0a276d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d134da3a11bc96f68cfd",
+    "url": "/Bosta/static/js/3.dbf8dec7.chunk.js"
+  },
+  {
+    "revision": "52fee876f4b057dbcb45",
+    "url": "/Bosta/static/js/main.5f1ba3e3.chunk.js"
+  },
+  {
+    "revision": "e69f44521bea7a550656",
+    "url": "/Bosta/static/js/runtime-main.d1f3f8ab.js"
+  },
+  {
+    "revision": "b994e5a9907948a1e16dcb0b6357aa97",
+    "url": "/Bosta/static/media/1.b994e5a9.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Bosta/static/media/logo.06e73328.svg"
+  }
+]);
